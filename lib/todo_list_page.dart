8edf22a2f096be410d89536640dfe50e5de434b9
William Borges <williammrborges@protonmail.com>
2022-01-10 14:17:07 -0300

    apagando teste

--- lib/todo_list_page.dart
+++ lib/todo_list_page.dart
@@ -7,8 +7,6 @@ class TodoListPage extends StatefulWidget {
   _TodoListPageState createState() => _TodoListPageState();
 }
 
-//teste
-
 class _TodoListPageState extends State<TodoListPage> {
   TextEditingController _textEditingController = TextEditingController();
   List<String> tarefas = [];
